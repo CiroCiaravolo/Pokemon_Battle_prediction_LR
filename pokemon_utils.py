77a308cf21693251c2bfdd064247217830ecfad7
CiroCiaravolo <131005811+CiroCiaravolo@users.noreply.github.com>
2025-11-16 17:17:50 +0100

    Add files via upload

---
+++ pokemon_utils.py
@@ -0,0 +1,16 @@
+'''function that returns names and stats of the pokemon we know '''
+def create_dict_team(names: list): 
+
+
+'''function that studies the pokemon alive in the last round and compares the types and their possible advantages on the types of the opponents'''
+# The lists contain the names of the remaining pokemon alive in the last turn
+# name1 = attacker's team, defender's team
+def battle_of_types(names1:list, names2:list):
+
+
+'''make a dictionary with the dynamic stats we know: hp level and status'''
+def latest_dict_stats(timeline, all_names1:dict , all_names2:dict) :
+
+
+''' count how many times the players switched pokemon '''
+def switch_count(timeline ):
